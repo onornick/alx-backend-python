eb5a1d1d6b6d084279135fa4786d62f314c9a25b
onornick <kiprotichnickson20@gmail.com>
2024-06-04 19:06:17 +0300

    changed the Union | to ,

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     takes a string k and an int OR float v as arguments and
     returns a tuple. The first element of the tuple is the
