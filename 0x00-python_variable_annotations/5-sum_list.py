08fc00e9320819df56ce8ae28c8532b9467cba96
onornick <kiprotichnickson20@gmail.com>
2024-06-04 15:02:53 +0300

    added documentation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module contains function: 'sum_list'
+"""
+
 
 def sum_list(input_list: list[float]) -> float:
+    """
+    Function sum_slist() takes in a list
+    of float values and returns the sum in float
+    """
     total = 0
     for num in input_list:
         total = total + num
