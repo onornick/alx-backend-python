d5918a7e6a71730d5aba1df208502313088c434d
onornick <kiprotichnickson20@gmail.com>
2024-06-10 00:31:01 +0300

    sorted the result in ascending order

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -14,4 +14,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     delays = await asyncio.gather(
             *tuple(map(lambda _: task_wait_random(max_delay), range(n))))
 
-    return delays
+    return sorted(delays)
