61968c4880628e37f94eb47fce6160f27d411c8c
onornick <kiprotichnickson20@gmail.com>
2024-06-09 16:37:00 +0300

    pycodestyle conformance

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
+"""
+This module imports a coroutine
+"""
 import asyncio
 from typing import List
-
 wait_random = __import__("0-basic_async_syntax").wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This function returns sorted delays of coroutines
+    """
+
     coroutines = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*coroutines)
-    
     sorted_delays = []
-
     while delays:
         min_delay = min(delays)
         sorted_delays.append(min_delay)
